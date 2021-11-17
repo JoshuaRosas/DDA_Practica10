5ea955b514bd502ac9aa644c35e332044b3b211a
Jeremy Huaringa <jeremy.huaringa@ucsm.edu.pe>
2021-11-16 21:37:38 -0500

    Experiencia 2

---
+++ Experiencia_2/Controller/inicio.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class ejemplo
+{
+public function inicio()
+{
+return new Response("Mensaje: hola gente!!!");
+}
+}
