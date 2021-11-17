536be706f7e83b574cafb52e99ff7ee7b6299fa7
Jeremy Huaringa <jeremy.huaringa@ucsm.edu.pe>
2021-11-16 21:40:28 -0500

    Experiencia 3

---
+++ Experiencia 3/Entity/Autores.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Autores
+ *
+ * @ORM\Table(name="autores")
+ * @ORM\Entity
+ */
+class Autores
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="text", length=65535, nullable=false)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="text", length=65535, nullable=false)
+     */
+    private $author;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="year", type="date", nullable=false)
+     */
+    private $year;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getYear(): ?\DateTimeInterface
+    {
+        return $this->year;
+    }
+
+    public function setYear(\DateTimeInterface $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+
+}
