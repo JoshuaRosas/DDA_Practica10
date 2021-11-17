0f91378199546afb0823f1aef28c096197c4157e
Jeremy Huaringa <jeremy.huaringa@ucsm.edu.pe>
2021-11-16 21:31:19 -0500

    Experiencia 1

---
+++ Experiencia 1/C) AISLANDO LA LÓGICA DE LA APLICACIÓN (EL DOMINIO)/model.php
@@ -0,0 +1,29 @@
+<?php
+//model.php
+function open_database_connection()
+{
+ $link = mysqli_connect("localhost", "root", "", "blog_db");
+ /* verificar la conexión */
+ if (mysqli_connect_errno()) {
+ printf("Conexión fallida: %s\n", mysqli_connect_error());
+ exit();
+ }
+ return $link;
+}
+function close_database_connection($link)
+{
+ mysql_close($link);
+}
+function get_all_posts()
+{
+ $link =open_database_connection();
+ $consulta = "SELECT id, title from post";
+ $result = mysqli_query($link, $consulta);
+ $posts = array();
+ while($row = mysqli_fetch_assoc($result)){
+ $posts[] = $row;
+ }
+ close_database_connection($link);
+ return $posts;
+}
+
---
+++ Experiencia 1/D) AISLANDO EL DISEÑO/list.php
@@ -0,0 +1,14 @@
+<?php $title = 'List of Post1' ?>
+<?php ob_start() ?>
+ <h1>List of Post1</h1>
+ <ul>
+ <?php foreach ($posts as $post): ?>
+ <li>
+ <a href="/read?id=<?php echo $post['id']?>">
+ <?php echo $post['title']?>
+ </a>
+ </li>
+ <?php endforeach; ?>
+ </ul>
+ <?php $content = ob_get_clean() ?>
+ <?php include 'base.php' ?>
